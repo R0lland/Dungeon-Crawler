21ea9444d2b2303ce3e4602fa83716110e803dd8
Mateus Trombetta <trombetta.mateus@gmail.com>
2024-01-05 16:28:43 -0300

    Added srand on main to truly randomize numbers for enemies

--- Dungeon Crawler/Dungeon Crawler/DungeonCrawler.cpp
+++ Dungeon Crawler/Dungeon Crawler/DungeonCrawler.cpp
@@ -102,12 +102,14 @@ int main()
 {
     std::string userName;
     int numberOfEnemies;
-
+    
     std::cout << "Type your name: ";
     std::cin >> userName;
 
     std::cout << "Hello " << userName << "!" << std::endl << "How many enemies you want to face?" << std::endl;
     std::cin >> numberOfEnemies;
+
+    srand(time(0));
 
     Character character = Character(userName);
 
